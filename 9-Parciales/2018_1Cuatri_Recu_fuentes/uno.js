ca23dfceeccc5157e89c2090eb0a1bf256a2b814
alumno <alumno>
2019-02-15 09:39:23 -0300

    Ejercicio 1 practico resuelto

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
@@ -1,5 +1,24 @@
 
 function mostrar()
 {
+var mascota1;
+var mascota2;
+var peso1;
+var peso2;
+var suma;
+
+
+ mascota1 = "Labrador";
+ mascota2 = "Pitbull";
+
+ peso1 = prompt("Ingresar el peso de su "+mascota1);
+ peso2 = prompt("Ingresar el peso de su "+mascota2);
+
+ peso1 = parseInt(peso1);
+ peso2 = parseInt(peso2);
+
+ suma = peso1 + peso2 ; 
+
+ alert ("tenes dos mascotas "+mascota1+" y "+mascota2+"Que pesan "+peso1+"y "+peso2+" kilos y la suma es "+suma);
 
 }
