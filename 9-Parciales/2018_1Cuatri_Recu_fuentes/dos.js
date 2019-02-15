4aea9c98cf4a70e357c8877fd12e508bb9982859
alumno <alumno>
2019-02-15 09:57:41 -0300

    Ejercicio 2

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/dos.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/dos.js
@@ -1,4 +1,26 @@
 function mostrar()
 {
-  
+
+var compra;
+var descuento;
+var total;
+var iva ;
+var totaldesc;
+
+compra = prompt ("Ingresar valor de compra ");
+
+compra = parseInt(compra);
+
+descuento = compra*10/100;
+
+totaldesc = compra - descuento;
+
+iva = totaldesc*21/100;
+
+total = iva + totaldesc
+
+alert ("tu compra es de $"+compra+" tenes un descuento del 10% queda en $ "+totaldesc+" mas el iva es $ "+total);
+
+
+
 }
